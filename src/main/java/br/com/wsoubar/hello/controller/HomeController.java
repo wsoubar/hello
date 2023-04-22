087b47d26ca7c6055bc9b746d0620f74e1c84d6b
Wagner Barbosa <wsoubar@gmail.com>
2023-04-21 21:28:57 -0300

    correção do ajax

--- src/main/java/br/com/wsoubar/hello/controller/HomeController.java
+++ src/main/java/br/com/wsoubar/hello/controller/HomeController.java
@@ -3,14 +3,14 @@ package br.com.wsoubar.hello.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wsoubar.hello.vo.HelloVO;
 
 @RestController
 public class HomeController {
-    
-    @CrossOrigin(origins = "http://localhost:8080")
+    //@CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/home")
     public ResponseEntity<?> name() {
         HelloVO vo = new HelloVO();
--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,7 +1,7 @@
 $(document).ready(new function(){
     //console.log("Carregou JQuery Ready. " + new Date());
     $("#btnSearch").on('click', function(e){
-        var keyword = $("#txSearch").val();
+        var keyword = $("#txSearch").val().escape();
         alert("Buscar por: "+ keyword);
     });
 
@@ -10,12 +10,29 @@ $(document).ready(new function(){
         //alert("antes do ajax. " + CONTEXT_PATH );
         alert("antes do ajax. ");
         $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/SampleApp/home',
+            data: {
+                text: 'hello'
+            },
+            success: function(text) {
+                console.log('Sucesso: ' + JSON.stringify(text));
+                console.log("nome: " + text.nome);
+                console.log("sobrenome: "+ text.sobrenome);
+            },
+            error: function(jqXHR) {
+                console.log('Error: ' + jqXHR.status);
+            }
+        });        
+            /*
+        $.ajax({
             type: "GET",
             contextType: "application/json",
-            url: "https://dummyjson.com/carts",
+            url: "http://localhost:8080/SampleApp/home",
         }).then(function (data, status, jqxhr) {
             alert("oooiii");
         });
+        &*/
     });
     /*
             dataType: "json",
